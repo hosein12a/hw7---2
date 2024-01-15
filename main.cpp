cd025db975c9a4bd191d65cbe95b5a6d24a9d0b1
hosein12a <153413720+hosein12a@users.noreply.github.com>
2024-01-15 11:25:40 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+#include <iostream> 
+using namespace std; 
+ 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */ 
+ 
+int main() { 
+int m,n,i; 
+cout <<"please enter m= "; 
+cin>>m; 
+cout<<"please enter n= "; 
+cin>>n; 
+for(i=0;n<=m;i++){ 
+ m=m-n; 
+} 
+cout<<"baghimande= "<<m<<"kharege gesmat= "<<i; 
+}
